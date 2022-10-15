77ea6861cd801d48954c4f6633058997c958a17d
Raviraj Kumar <ravirajk545@gmail.com>
2022-10-15 21:38:26 +0530

    adding recursing

---
+++ recursionFactorial.cpp
@@ -0,0 +1,24 @@
+// WAP to find the factorial of any number using recursion 
+
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter any no. ";
+    cin>>n;
+
+    cout<<factorial(n);
+
+    return 0;
+}
---
+++ tem.js
@@ -0,0 +1,2 @@
+console.log("Raviraj\tTiwari\n")
+//prompt("Enter your age")
