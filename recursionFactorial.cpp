0864e1d260818a1f522de352483c5f53ba7d2b82
Raviraj Kumar <ravirajk545@gmail.com>
2022-10-16 11:45:23 +0530

    adding 1. Factorial of any number using recursion 2. 2^n using recursion 3. Counting

--- recursionFactorial.cpp
+++ recursionFactorial.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int factorial(int n)
 {
-    if(n == 0)
+    if(n == 0)// base case
     {
         return 1;
     }
-    return n * factorial(n - 1);
+    return n * factorial(n - 1);// recursive relation
 }
 
 int main()
