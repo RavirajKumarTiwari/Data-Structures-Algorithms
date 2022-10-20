6bf68e45951a262ee2024bba37f8d9f5c2c43798
Raviraj Kumar <ravirajk545@gmail.com>
2022-10-20 19:57:16 +0530

    LeetCode problem Fibonacci Number solution using recursion

---
+++ fibonacciNumber.cpp
@@ -0,0 +1,28 @@
+// 509. Fibonacci Number (LeetCode)
+
+//The Fibonacci numbers,commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+//------------solution----------------
+#include<iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    int ans = fib(n - 1) + fib(n - 2);
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of 'n' : ";
+    cin>>n;
+
+    cout<<fib(n);
+}
