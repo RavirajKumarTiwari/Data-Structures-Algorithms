d9cb299a98209b0a4920310203759552133f42ac
Raviraj Kumar <ravirajk545@gmail.com>
2022-10-20 20:37:54 +0530

    including question text

--- CountWaysToReachThe_Nth_Stairs.cpp
+++ CountWaysToReachThe_Nth_Stairs.cpp
@@ -1,8 +1,14 @@
 //Count Ways To Reach The N - th Stairs (Codestudio) by coding Ninja
 
+//Question:-
+
+// You are climbing a staircase.It takes n steps to reach the top.Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top?
+
+//solution :- 
+
 #include <bits/stdc++.h>
-//#include <iostream>
-using namespace std;
+    //#include <iostream>
+    using namespace std;
 int countDistinctWays(int nStairs)
 {
     // base case
